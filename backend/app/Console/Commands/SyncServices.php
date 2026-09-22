142f800f74d29b57767d4a58cb9bd6976426d050
dllikrnwn <faadlikurniawan9@gmail.com>
2026-09-22 22:28:18 +0700

    booking: slot jam kelipatan 15 menit + Full Legs terakhir pukul 18:00

--- backend/app/Console/Commands/SyncServices.php
+++ backend/app/Console/Commands/SyncServices.php
@@ -26,7 +26,7 @@ class SyncServices extends Command
         ['name' => 'Full Front', 'category' => 'upper', 'duration_minutes' => 30, 'wax_type' => 'Gentle Film Hard Wax', 'description' => 'Waxing bulu area dada & perut'],
         ['name' => 'Full Back', 'category' => 'upper', 'duration_minutes' => 30, 'wax_type' => 'Gentle Film Hard Wax', 'description' => 'Waxing bulu area punggung'],
         ['name' => 'Half Legs', 'category' => 'legs', 'duration_minutes' => 15, 'wax_type' => 'Organic Soft Honey', 'description' => 'Waxing area lutut hingga ujung kaki'],
-        ['name' => 'Full Legs', 'category' => 'legs', 'duration_minutes' => 30, 'wax_type' => 'Organic Soft Honey', 'description' => 'Waxing area paha hingga ujung kaki. Untuk pria, cakupan area hanya ¾ kaki', 'last_order_time' => '18:30'],
+        ['name' => 'Full Legs', 'category' => 'legs', 'duration_minutes' => 30, 'wax_type' => 'Organic Soft Honey', 'description' => 'Waxing area paha hingga ujung kaki. Untuk pria, cakupan area hanya ¾ kaki', 'last_order_time' => '18:00'],
         ['name' => 'Basic Bikini', 'category' => 'intimate', 'duration_minutes' => 15, 'wax_type' => 'Gentle Film Hard Wax', 'description' => 'Waxing bulu area bikini line'],
         ['name' => 'Brazilian', 'category' => 'intimate', 'duration_minutes' => 30, 'wax_type' => 'Gentle Film Hard Wax', 'description' => 'Waxing area intim menyeluruh'],
         ['name' => 'Buttocks', 'category' => 'intimate', 'duration_minutes' => 15, 'wax_type' => 'Gentle Film Hard Wax', 'description' => 'Waxing bulu area bokong'],
